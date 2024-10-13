8b37d46b8c90ef61970c3480ab25b7575b4983e7
Martin Joey <joey.calypso@gmail.com>
2024-10-13 20:15:57 +0200

    add burger menu and touch the header CSS

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuBurger = document.querySelector('.menu_burger');
+const closeBurger = document.querySelector('.close_burger');
+const navigation = document.querySelector('.navigation')
+
+function openMenu() {
+    navigation.classList.add('open');
+    menuBurger.style.display = 'none';
+    closeBurger.style.display = 'block';
+}
+
+function closeMenu() {
+    navigation.classList.remove('open');
+    menuBurger.style.display = 'block';
+    closeBurger.style.display = 'block';
+}
+
+menuBurger.addEventListener('click', openMenu);
+closeBurger.addEventListener('click',closeMenu);
