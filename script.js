a0b42635f1e816db27dea08befbec20ba6746eee
Martin Joey <joey.calypso@gmail.com>
2024-10-16 11:51:38 +0200

    fix burger menu and add flag for languages

--- script.js
+++ script.js
@@ -24,33 +24,5 @@ function closeMenu() {
 menu_burger.addEventListener('click', openMenu);
 close_burger.addEventListener('click',closeMenu);
 
-/*function closeMenu() {
-
-    navigation.classList.add('close');
-
-    if(close) {
-        menu_burger.style.display= "block";
-        close_burger.style.display= "none";
-    }
-    else {
-        menu_burger.style.display= "none";
-        close_burger.style.display= "block";
-    }
-}*/
 
 
-
-/*const menuBurger = document.querySelector('.menu_burger');
-const closeBurger = document.querySelector('.close_burger');
-const navigation = document.querySelector('.navigation')
-
-function openMenu() {
-    navigation.classList.add('open');
-}
-
-function closeMenu() {
-    navigation.classList.remove('open');
-}
-
-menuBurger.addEventListener('click', openMenu);
-closeBurger.addEventListener('click',closeMenu); */
