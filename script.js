f37685478822f311ed95ce82219fc59db9b8b8a6
Martin Joey <joey.calypso@gmail.com>
2024-10-16 11:29:26 +0200

    fix menu burger and add flag languages

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
-const menuBurger = document.querySelector('.menu_burger');
+const menu_burger = document.querySelector('.menu_burger');
+const close_burger = document.querySelector('.close_burger');
+const navigation = document.querySelector('.navigation')
+
+function openMenu() {
+
+    navigation.classList.add('open');
+
+        menu_burger.style.display= "none";
+        close_burger.style.display= "block";
+    
+    
+}
+
+function closeMenu() {
+
+    navigation.classList.remove('open')
+
+        menu_burger.style.display= "block";
+        close_burger.style.display= "none";
+
+}
+
+menu_burger.addEventListener('click', openMenu);
+close_burger.addEventListener('click',closeMenu);
+
+/*function closeMenu() {
+
+    navigation.classList.add('close');
+
+    if(close) {
+        menu_burger.style.display= "block";
+        close_burger.style.display= "none";
+    }
+    else {
+        menu_burger.style.display= "none";
+        close_burger.style.display= "block";
+    }
+}*/
+
+
+
+/*const menuBurger = document.querySelector('.menu_burger');
 const closeBurger = document.querySelector('.close_burger');
 const navigation = document.querySelector('.navigation')
 
 function openMenu() {
     navigation.classList.add('open');
-    menuBurger.style.display = 'none';
-    closeBurger.style.display = 'block';
 }
 
 function closeMenu() {
     navigation.classList.remove('open');
-    menuBurger.style.display = 'block';
-    closeBurger.style.display = 'block';
 }
 
 menuBurger.addEventListener('click', openMenu);
-closeBurger.addEventListener('click',closeMenu);
+closeBurger.addEventListener('click',closeMenu); */
